0d62bc3b7b8b6abbb15cc527a280cd41973b61ee
Recal <50343845+Recal@users.noreply.github.com>
2022-04-11 16:19:02 -0500

    Update update-jdk.sh

--- update-jdk.sh
+++ update-jdk.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install openjdk-17-jdk
+sudo apt-get install -y openjdk-17-jdk
 PATH=$(echo $PATH | sed -e 's;:\?/home/gitpod/.sdkman/candidates/java/current/bin;;')
