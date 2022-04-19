492ea928dea4bdf1a8913db3e1f0aecc0c3ca998
Recal <50343845+Recal@users.noreply.github.com>
2022-04-19 09:48:05 -0500

    Update update-jdk.sh

--- update-jdk.sh
+++ update-jdk.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-17-jdk
-#sdk uninstall java 11.0.13.fx-zulu //  Base installed java version on Gitpod
+sdk uninstall java 11.0.13.fx-zulu # Base installed java version on Gitpod
 export PATH=$(echo $PATH | sed -e 's;:/home/gitpod/.sdkman/candidates/java/current/bin;;')
